79c4a65a70be3e405b852b25bc00ae1a217e058b
陳阿仔 <hahabon@gmail.com>
2023-10-26 22:25:24 +0800

    feat(dark-mode): add dark mode

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import _ from 'lodash'
+import DarkModeSwitcher from '../components/DarkModeSwitcher'
 </script>
 
 <template>
   <h2 class="mt-10 text-lg font-medium intro-y">首頁</h2>
   <div>
     <br />
+    <DarkModeSwitcher />
     <div>{{ $t('hello') }} 登入頁面 /dashboard/login</div>
   </div>
 </template>
