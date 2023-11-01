352cf3ef82053729e8fa7f83841a78bbe6a8f3b8
陳阿仔 <hahabon@gmail.com>
2023-11-02 06:50:18 +0800

    fix(design-template): fix build error

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,42 @@
 const plugin = require('tailwindcss/plugin')
 const colors = require('tailwindcss/colors')
 const { parseColor } = require('tailwindcss/lib/util/color')
-import DesignTemplate from './src/types/design-template'
 
+const DesignTemplate = {
+  main: {
+    theme1: '#02513F',
+    theme2: '#E3F2A5',
+    theme3: '#F4FABD',
+    theme4: '#1D3D0E',
+    disabled_bg: '#02B13F', // opactiy 30%
+    disabled_font: '#7B7B7B',
+    warning: '#FF4D4F',
+    html_bg: '#FFFFFF',
+    dashboard_bg: '#F6F6F6',
+    content_bg: '#FFFFFF',
+    input_bg: '#F6F6F6',
+    font: '#2D3748',
+    desc_font: '#939393',
+    placeholder_font: '#000000' // opactiy 25%
+  },
+  dark: {
+    theme1: '#009E37',
+    theme2: '#D7DFB3',
+    theme3: '#E9ECD0',
+    theme4: '#1D3D0E',
+    disabled_bg: '#31383F',
+    disabled_font: '#7B7B7B',
+    warning: '#FF4D4F',
+    html_bg: '#FFFFFF',
+    // 'dashboard-bg':'#1D2539'
+    dashboard_bg: '#252D43',
+    content_bg: '#2A334C',
+    input_bg: '#1D2539',
+    font: '#FFFFFF', // opactiy 87%
+    desc_font: '#939393',
+    placeholder_font: '#000000' // opactiy 25%
+  }
+}
 /** Converts HEX color to RGB */
 const toRGB = (value) => {
   return parseColor(value).color.join(' ')
