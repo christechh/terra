708302d9c24b30c763a3e7691f3318538868793e
Howard Sun <howardsun@csie.io>
2023-11-01 20:10:53 +0800

    feat: add firebsae

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dashboard/',
   build: {
     commonjsOptions: {
       include: ['tailwind.config.js', 'node_modules/**']
