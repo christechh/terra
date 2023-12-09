e03a0ad8c505c9a8af18374fe2b622f3ecca08db
ean <j19900313@gmail.com>
2023-12-10 01:51:06 +0800

    refactor: refact link info

---
+++ src/components/VerticalSteps/Step.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+const props = defineProps({
+  step: {
+    type: Number,
+    required: true
+  },
+  isFinal: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+<template>
+  <li class="pb-5 ps-6" :class="{ 'border-s border-[#E3F2A5]': !isFinal }">
+    <span
+      class="absolute -start-3 flex h-6 w-6 items-center justify-center rounded-full bg-primary text-white ring-1 ring-white dark:bg-blue-900 dark:ring-gray-900"
+    >
+      {{ props.step }}
+    </span>
+    <slot></slot>
+  </li>
+</template>
